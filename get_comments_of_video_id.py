a7af71b540c0a5d53826fcb71ab2720ec7d21015
William <xwilliamy@gmail.com>
2020-06-27 21:21:54 -0400

    Scrape comments from a video until you reach your daily quota

---
+++ get_comments_of_video_id.py
@@ -0,0 +1,171 @@
+import argparse
+import json
+import pandas as pd
+
+from apiclient.discovery import build
+from csv import writer
+from urllib.parse import urlparse, parse_qs
+
+def get_keys(filename):
+    with open(filename) as f:
+        key = f.readline()
+    DEVELOPER_KEY = key
+    YOUTUBE_API_SERVICE_NAME = "youtube"
+    YOUTUBE_API_VERSION = "v3"
+    return {'key': key, 'name': 'youtube', 'version': 'v3'}
+
+def build_service(filename):
+    api_dict = get_keys(filename)
+    return build(api_dict['name'],
+                 api_dict['version'],
+                 developerKey=api_dict['key'])
+
+def get_videos(**kwargs):
+    final_results = []
+    service = kwargs['service']
+
+    kwargs['part'] = kwargs.get('part', 'id,snippet')
+
+    search_response = service.videos().list(
+        **kwargs
+    ).execute()
+
+    i = 0
+    max_pages = 3
+    while search_response and i < max_pages:
+        final_results.extend(search_response['items'])
+
+        if 'nextPageToken' in search_response:
+            kwargs['pageToken'] = search_response['nextPageToken']
+            search_response = service.search().list(
+                **kwargs
+                ).execute()
+            i += 1
+        else:
+            break
+    return final_results
+
+# https://stackoverflow.com/questions/45579306/get-youtube-video-url-or-youtube-video-id-from-a-string-using-regex
+def get_id(url):
+    u_pars = urlparse(url)
+    quer_v = parse_qs(u_pars.query).get('v')
+    if quer_v:
+        return quer_v[0]
+    pth = u_pars.path.split('/')
+    if pth:
+        return pth[-1]
+
+def get_comments(**kwargs):
+    """
+    ty: 
+    https://python.gotrained.com/youtube-api-extracting-comments/#Cache_Credentials
+    https://www.pingshiuanchua.com/blog/post/using-youtube-api-to-analyse-youtube-comments-on-python
+    """
+    comments, commentsId, repliesCount, likesCount, viewerRating = [], [], [], [], []
+
+    # clean kwargs
+    kwargs['part'] = kwargs.get('part', 'snippet')
+    kwargs['maxResults'] = kwargs.get('maxResults', 100)
+    kwargs['textFormat'] = kwargs.get('textFormat', 'plainText')
+    kwargs['order'] = kwargs.get('order', 'time')
+    iterations = kwargs.pop('iterations', None)
+    write_lbl = kwargs.pop('write_lbl', True)
+    csv_filename = kwargs.pop('csv_filename')
+    token_filename = kwargs.pop('token_filename')
+
+    count = 0
+    service = kwargs.pop('service')
+    
+    response = service.commentThreads().list(
+        **kwargs
+    ).execute()
+
+    while response:
+        print(count)
+        if iterations and count == iterations:
+            return {
+                'Comments': comments,
+                'Comment ID' : commentsId,
+                'Reply Count' : repliesCount,
+                'Like Count' : likesCount,
+                'Commenter Rating Video' : viewerRating
+                }
+        else:
+            count += 1
+            
+        for item in response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            comment_id = item['snippet']['topLevelComment']['id']
+            reply_count = item['snippet']['totalReplyCount']
+            like_count = item['snippet']['topLevelComment']['snippet']['likeCount']
+
+            viewer_rating = item['snippet']['topLevelComment']['snippet']['viewerRating']
+            
+            comments.append(comment)
+            commentsId.append(comment_id)
+            repliesCount.append(reply_count)
+            likesCount.append(like_count)
+            viewerRating.append(viewer_rating)
+
+            if write_lbl == True:
+                with open(f'{csv_filename}.csv', 'a+') as f:
+                    # https://thispointer.com/python-how-to-append-a-new-row-to-an-existing-csv-file/#:~:text=Open%20our%20csv%20file%20in,in%20the%20associated%20csv%20file
+                    csv_writer = writer(f)
+                    csv_writer.writerow([comment, comment_id, reply_count, like_count, viewer_rating])
+
+            if 'nextPageToken' in response:
+
+                with open(f'{token_filename}.txt', 'a+') as f:
+                    f.write(kwargs.get('pageToken', ''))
+                    f.write('\n')
+                kwargs['pageToken'] = response['nextPageToken']
+                response = service.commentThreads().list(
+                    **kwargs
+                ).execute()
+            else:
+                return {
+                    'Comments': comments,
+                    'Comment ID' : commentsId,
+                    'Reply Count' : repliesCount,
+                    'Like Count' : likesCount,
+                    'Commenter Rating Video' : viewerRating
+                }
+
+def save_to_csv(output_dict, video_id, output_filename):
+    output_df = pd.DataFrame(output_dict, columns = output_dict.keys())
+    output_df.to_csv(f'{output_filename}.csv')
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--part', help='Desired properties of commentThread', default='snippet')
+    parser.add_argument('--maxResults', help='Max results per page', default=100)
+    parser.add_argument('--iterations', help='How many pages to parse', default=None)
+    parser.add_argument('--write_lbl', help="Update csv after each comment?", default=True)
+    parser.add_argument('--csv_filename', default=None)
+    parser.add_argument('--token_filename', default=None)
+    parser.add_argument('--video_url', default='https://www.youtube.com/watch?v=ioNng23DkIM')
+    args = parser.parse_args()
+
+    # maybe use argparse or something idk
+
+    service = build_service('../apikey.json')
+    video_id = get_id(args.video_url)
+    # part, maxResults, textFormat, iterations, service, write_lbl, csv_filename, token_filename
+
+    if not args.csv_filename:
+        args.csv_filename = video_id + "_csv"
+
+    if not args.token_filename:
+        args.token_filename = video_id + "_page_token"
+
+    kwargs = vars(args)
+    kwargs.pop('video_url')
+    kwargs['videoId'] = video_id
+    kwargs['service'] = service
+    output_dict = get_comments(**kwargs)
+    save_to_csv(output_dict, video_id)
+    
+if __name__ == '__main__':
+    # do the things
+    main()
